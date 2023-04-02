6058c2e55e67538de75e5c79ce12f57468b24112
draganlukic1527 <draganlukic1527@gmail.com>
2023-04-02 01:28:43 -0700

    check

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const path = require('path');
-const store = require('./store/datastore');
-const initialStoreData = require('./store/data');
-const Musician = require('./models/musician');
-const musicianRoutes = require('./routes/musician');
+const express = require("express");
+const path = require("path");
+const store = require("./store/datastore");
+const initialStoreData = require("./store/data");
+const Musician = require("./models/musician");
+const musicianRoutes = require("./routes/musician");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-// include routes
-app.use('/musician', musicianRoutes);
+// include routes check
+app.use("/musician", musicianRoutes);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Index route
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 // initialize store
@@ -25,7 +25,7 @@ app.locals.musician = musician;
 
 // start server
 const server = app.listen(port, () => {
-  console.log("Server started on port " + port);
+	console.log("Server started on port " + port);
 });
 
 module.exports = server;
